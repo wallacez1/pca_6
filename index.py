0b59aafc2ae350748c0c459eb212d234040db809
wallaceSilvaz1 <wallace.silva@qwst.co>
2019-11-11 14:21:37 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+filmes = pd.read_csv('./data.csv')
+
+filmes = filmes.dropna()
+
+filmes.rename(columns={"Nome": "nome", "Classificação(Anderson)": "nota",
+                       "Tipo": "tipo", "Categoria": "categoria"}, inplace=True)
+
+del filmes['Orçamento']
+del filmes['Diretor']
+del filmes['nome']
+
+
+X = pd.get_dummies(
+    filmes, prefix=['tipo', 'categoria'], columns=['tipo', 'categoria'])
+
+del X['nota']
+
+Y = pd.get_dummies(
+    filmes, prefix=['nota'], columns=['nota'])
+
+del Y['categoria']
+del Y['tipo']
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.3, random_state=27)
+
+
+clf = RandomForestClassifier()
+
+# # Train Decision Tree Classifer
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
